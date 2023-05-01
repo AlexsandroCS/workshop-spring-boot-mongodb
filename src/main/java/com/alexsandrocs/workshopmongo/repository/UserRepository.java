461139bb6ba3ef35aaf12cfea08a36f5790ed97e
AlexsandroCS <alexsandro.acs98@gmail.com>
2023-05-01 10:22:43 -0300

    Connecting to MongoDB with repository and service

---
+++ src/main/java/com/alexsandrocs/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.alexsandrocs.workshopmongo.repository;
+
+import com.alexsandrocs.workshopmongo.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+
+}
