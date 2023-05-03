f78efe9602e9d2b5f6953922f5cbaf261ebba555
AlexsandroCS <alexsandro.acs98@gmail.com>
2023-05-03 10:45:53 -0300

    Inserting comments in post

--- src/main/java/com/alexsandrocs/workshopmongo/config/Instantiation.java
+++ src/main/java/com/alexsandrocs/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.alexsandrocs.workshopmongo.config;
 import com.alexsandrocs.workshopmongo.domain.Post;
 import com.alexsandrocs.workshopmongo.domain.User;
 import com.alexsandrocs.workshopmongo.dto.AuthorDTO;
+import com.alexsandrocs.workshopmongo.dto.CommentDTO;
 import com.alexsandrocs.workshopmongo.repository.PostRepository;
 import com.alexsandrocs.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,6 +39,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2023"),"Partiu viagem!","Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null,sdf.parse("23/03/2023"),"Bom dia!","Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!",sdf.parse("21/03/2023"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!",sdf.parse("22/03/2023"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!",sdf.parse("23/03/2023"), new AuthorDTO(bob));
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
