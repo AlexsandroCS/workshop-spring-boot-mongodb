d2e8a64ea06389a525dbe1746a5259e48000f41f
AlexsandroCS <alexsandro.acs98@gmail.com>
2023-05-03 11:39:20 -0300

    Simple query with query methods

--- src/main/java/com/alexsandrocs/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/alexsandrocs/workshopmongo/repository/PostRepository.java
@@ -1,11 +1,12 @@
 package com.alexsandrocs.workshopmongo.repository;
 
 import com.alexsandrocs.workshopmongo.domain.Post;
-import com.alexsandrocs.workshopmongo.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
